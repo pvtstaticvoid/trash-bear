fc6385725651e5071f4b679d5ec0d4cd3159343c
Roger Liu <pvtstaticvoid@gmail.com>
2022-10-15 23:48:00 -0400

    Implement getting minimum distances.

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ class Board extends React.Component {
                         </div>
                     )
                 })}
+                <button onClick={() => {this.props.onStart(0, 0)}}>Start!</button>
             </div>
         );
     }
@@ -46,14 +47,46 @@ class Board extends React.Component {
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        const squares = Array(100).fill(null);
+        const distances = Array(100).fill(2);
+        // squares[0] = "X";
+        // squares[99] = "X";
         this.state = {
-            squares: Array(100).fill(null)
+            squares: squares
         };
+    }
+
+    getDistances() {
+        const distances = Array(100).fill(null);
+        this.prop(0, 0, 0, distances)
+        this.setState({squares: distances});
+    }
+
+    prop(i, j, d, distances) {
+
+        const idx = i*10 + j;
+        if (i < 0 || i >= 10 || j < 0 || j >= 10) {
+            return;
+        }
+        if ((distances[idx] != null && distances[idx] <= d) || this.state.squares[idx] === "W") {
+            return;
+        }
+
+        if (idx === 0) {
+            console.log({idx}, {d}, distances[idx]);
+        }
+        
+        distances[idx] = d;
+
+        this.prop(i + 1, j, d + 1, distances);
+        this.prop(i - 1, j, d + 1, distances);
+        this.prop(i, j + 1, d + 1, distances);
+        this.prop(i, j - 1, d + 1, distances);
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? "X" : "O";
+        squares[i] = this.state.xIsNext ? "X" : "W";
         this.setState({
             squares: squares
         });
@@ -74,6 +107,10 @@ class Game extends React.Component {
                     <Board
                         squares={this.state.squares}
                         onClick={i => this.handleClick(i)}
+                        onStart={(i, j) => {
+                            this.getDistances();
+                            // this.setState({squares: this.distances});
+                        }}
                     />
                 </div>
                 <div className="game-info">
