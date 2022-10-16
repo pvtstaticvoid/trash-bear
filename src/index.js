cac973ee07d0869c67a632e543bc610085ae4f7d
Roger Liu <pvtstaticvoid@gmail.com>
2022-10-15 21:49:45 -0400

    Implement a 10x10 clickable grid.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,90 @@ import './index.css';
 
 // ========================================
 
-class Game extends React.Component {
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
     render() {
+        const idxs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         return (
             <div>
-                Hello, world!
+                {idxs.map((i) => {
+                    return (
+                        <div className="board-row">
+                            {idxs.map((j) => {
+                                return (
+                                    this.renderSquare(i*10 + j)
+                                );
+                            })}
+                        </div>
+                    )
+                })}
             </div>
         );
     }
 }
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(100).fill(null)
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares
+        });
+    }
+
+    render() {
+
+        // let status;
+        // if (winner) {
+        //     status = "Winner: " + winner;
+        // } else {
+        //     status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        // }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={this.state.squares}
+                        onClick={i => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    {/* <div>{status}</div> */}
+                    {/* <ol>{moves}</ol> */}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+
 
 // ========================================
 
